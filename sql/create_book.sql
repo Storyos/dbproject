51495a4d487655b1e4dacfd456c68b9c0072fd8d
Gaeol <ahffkd29@gmail.com>
2024-02-14 15:28:32 +0900

    sge_insert작업

--- sql/create_book.sql
+++ sql/create_book.sql
@@ -8,3 +8,9 @@ CREATE TABLE `Book` (
 	`book_id`	INT	NOT NULL,
 	`UID`	VARCHAR(50)	NOT NULL
 );
+
+INSERT INTO book
+VALUES (123456, '마흔에 읽는 쇼펜하우어', '강용수', '유노북스','2024-02-14', 2, '2024-02-14', 'sge1234') ;
+
+INSERT INTO book
+VALUES (123457, '트래픽설계자', '러셀 브런슨', '윌북','2024-02-10', 5, '2024-02-14', 'sge1235') ;
