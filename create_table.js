4d99b84705bffbc0c01d79dd4f31654085625983
GITJUSTDOIT <kimsunwoo141@naver.com>
2024-02-14 15:51:58 +0900

    ksw_ write create sql in create js

--- create_table.js
+++ create_table.js
@@ -13,7 +13,15 @@ connection.connect((err) => {
     if (err) return console.error(err.message);
 
     //여기에 sql문 다 복붙하시면 됩니다
-    const createTodosTable = ``;
+    const createTodosTable = `CREATE TABLE USER(
+      UID	INT	NOT NULL	COMMENT '사용자식별ID',
+      UNUM	INT	NOT NULL	COMMENT '사용자 학번',
+      UNAME	VARCHAR(20)	NOT NULL	COMMENT '사용자 이름',
+      UPNO	INT	NOT NULL	COMMENT '사용자 연락처',
+      CHECKOUT	BOOLEAN	NOT NULL	COMMENT '사용자 대출 가능여부',
+      UPWD	VARCHAR(20)	NOT NULL	COMMENT '사용자 비밀번호',
+      PRIMARY KEY (UID)
+    )`;
 
     connection.query(createTodosTable, (err, results, fields) => {
         if (err) return console.log(err.message);
