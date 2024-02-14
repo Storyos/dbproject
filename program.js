8440bcda62922b39725c62146b90ddc779ccd086
Storyos <storyoser@pukyong.ac.kr>
2024-02-14 14:28:19 +0900

    KBG_로그인 기능 추가

--- program.js
+++ program.js
@@ -9,32 +9,43 @@ let connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
+async function loginmenu() {
+    while (true) {
+        console.log(`1. 로그인  2.회원가입 3.관리자접속`);
+        let login_menu = await Input.getUserInput();
+        if (login_menu === '1') {
+            console.log('ID 입력 : ');
+            let login_id = await Input.getUserInput();
+        // 여기서 sql 문 작성
+            let sql = `select password from user where uid=login_id`;
+            connection.query(sql, [true], (error, result, fields) => {
+                if (error) return console.error(error.message);
+                // 로그인 성공처리
+                if (result === 'login_id') {
+                    return 0;
+                } else {
+                    console.log('로그인 실패 ');
+                }
+            });
+        }
+        else if(login_menu === '3'){
+            return 1;
+        }
+    }
+}
+
 async function main() {
-    console.clear();
     connection.connect();
-    // while (true) {
-    //     console.log(`1. 로그인  2.회원가입`);
-    //     let login_menu = await Input.getUserInput();
-    //     if (login_menu === '1') {
-    //         console.log('ID 입력 : ');
-    //         let login_id = await Input.getUserInput();
-    //     여기서 sql 문 작성
-    //         let sql = `select password from user where uid=login_id`;
-    //         connection.query(sql, [true], (error, result, fields) => {
-    //             if (error) return console.error(error.message);
-    //             if (result === 'login_id') {
-    //                 break;
-    //     로그인 성공처리
-    //             } else {
-    //                 console.log('로그인 실패 ');
-    //             }
-    //         });
-    //     }
-    // }
+    console.clear();
+
+    // loginmenu()에서 return값을 받음 --> 성공일 때, 넘기기
+    let login_success = await loginmenu();
+
+    if(login_success=1){
+        console.log("SUCCESSED");
     while (true) {
         console.log(`1. 데이터입력 2.데이터수정 3.데이터삭제 4.목록  5.종료`);
         let menu = await Input.getUserInput();
-        console.log("여기까지 됨");
         if (menu === '1') {
             console.log('제목입력>');
             // 설정 필요
@@ -59,9 +70,8 @@ async function main() {
         await wait(1000);
         console.clear();
     };
-
+};
 };
 
 main();
-
 const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay));
