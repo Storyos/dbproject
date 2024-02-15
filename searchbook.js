cfcfa8e1ab4f6c595033bcfe33558aa9565d200c
GITJUSTDOIT <kimsunwoo141@naver.com>
2024-02-15 15:57:44 +0900

    ksw_searchbook 5개씩 나눠보기

--- searchbook.js
+++ searchbook.js
@@ -1,7 +1,7 @@
 const Input = require('./userInput');
 let mysql = require('mysql');
 async function searchbook(connection) {
-console.log("찾으시는 도서나 저자 이름을 입력해 주세요 : ");
+  console.log("찾으시는 도서나 저자 이름을 입력해 주세요 : ");
   // book_title을 입력받음
   let book_title = await Input.getUserInput();
   // 입력된 값이 해당 sql문에 있는지 확인
@@ -11,23 +11,25 @@ console.log("찾으시는 도서나 저자 이름을 입력해 주세요 : ");
 
     if (pwd_err) return console.error(pwd_err.message);
 
-  
+
 
     // 입력이 정확하면 book_result에 값이 들어간다
     if (book_result.length > 0) {
-      console.log( `순번 | 책 제목   |   책 저자   |   출판사    |   출판연도`);
-      for(let i = 0; i < book_result.length; i++) {
-        console.log(`${i+1} | ${book_result[i].book_title}   |   ${book_result[i].book_author}   |   ${book_result[i].publisher}   |   ${book_result[i].publishing_year}`);
+      console.log(`순번 | 책 제목   |   책 저자   |   출판사    |   출판연도`);
+      for (let i = 0; i < book_result.length; i++) {
+        console.log(`${i + 1} | ${book_result[i].book_title}   |   ${book_result[i].book_author}   |   ${book_result[i].publisher}   |   ${book_result[i].publishing_year}`);
+        // 5개씩 나눠 보기
+        if((i+1)%5===0) console.log();
       }
     } else {
-        console.log(`일치하는 도서를 찾을 수 없습니다.`);
+      console.log(`일치하는 도서를 찾을 수 없습니다.`);
     }
   });
-          // connection.query(sql, [true], (pwd_err,book_result,fields)=>{
-          //     if(pwd_err) return console.error(pwd_err.message);            
-          //     if(book_result[0].book_title === book_title || book_result[0].book_author === book_title)
-          //       console.log(book_result[0].book_title, "일치합니다");  
-          // });
+  // connection.query(sql, [true], (pwd_err,book_result,fields)=>{
+  //     if(pwd_err) return console.error(pwd_err.message);            
+  //     if(book_result[0].book_title === book_title || book_result[0].book_author === book_title)
+  //       console.log(book_result[0].book_title, "일치합니다");  
+  // });
 }
 
-module.exports = {searchbook};
+module.exports = { searchbook };
