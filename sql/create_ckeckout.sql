3198f8a323bbc3b052d0823fd4f399936fc474e0
Storyos <storyoser@pukyong.ac.kr>
2024-02-15 14:38:17 +0900

    KBG_대출 SQL문 추가

--- sql/create_ckeckout.sql
+++ sql/create_ckeckout.sql
@@ -1,7 +1,10 @@
-CREATE TABLE Ckeckout (
+CREATE TABLE Checkout (
+	`book_uid` int NOT NULL  COMMENT '도서 ID',
+    `user_uid` int not null  comment '사용자 ID',
 	`loan_date`	   DATE	    NOT NULL COMMENT '대출날짜',
 	`return_date`  DATE	    NOT NULL COMMENT '반납날짜',
 	`book_delay`   BOOLEAN	NOT NULL COMMENT '연체여부',
-	foreign key (book_uid) references Book(UID)
-    foreign key (user_uid) references USER(UID)	
+	foreign key (book_uid) references Book(UID),
+    foreign key (user_uid) references USER(UID),
+    primary key(book_uid,user_uid)book_uid
 );
