51999e399ecf7bf256774d5a70cdad285174320e
sgismine <sgismine@naver.com>
2024-02-15 14:29:39 +0900

    psw_

--- sql/create_ckeckout.sql
+++ sql/create_ckeckout.sql
@@ -2,6 +2,7 @@ CREATE TABLE Ckeckout (
 	`loan_date`	   DATE	    NOT NULL COMMENT '대출날짜',
 	`return_date`  DATE	    NOT NULL COMMENT '반납날짜',
 	`book_delay`   BOOLEAN	NOT NULL COMMENT '연체여부',
+	
 	foreign key (book_uid) references Book(UID)
     foreign key (user_uid) references USER(UID)	
 );
