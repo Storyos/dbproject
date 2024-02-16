38679565f1798b7fdfdda8eda317845c6846edd7
Storyos <storyoser@pukyong.ac.kr>
2024-02-16 14:00:04 +0900

    KBG_사용자조회기능 수정

--- userInfo.js
+++ userInfo.js
@@ -5,17 +5,11 @@ console.log("사용자 정보를 조회하시겠습니까?");
 async function userInfo(connection) {
   console.log("사용자 정보를 조회하시겠습니까?");
   
-  let info = `SELECT UID, UNUM, UNAME, UPNO, CHECKOUT, UPWD FROM USER`;
-
+  let info = `SELECT UID, UNUM 학번, UNAME 이름, UPNO 전화번호, CHECKOUT 대출가능여부 FROM USER`;
+    
   connection.query(info, async (error, results, fields) => {
     if (error) return console.error(error.message);
-    
-    let info = `SELECT UID, UNUM, UNAME, UPNO, CHECKOUT, UPWD FROM USER`;
-    
-    if (error) return console.error(error.message);
-      for(let i = 0; i < results.length; i++) {
-          console.log(`${i+1} | ${results[i].UID}   |   ${results[i].UID}   |   ${results[i].UNUM}   |   ${results[i].UNAME}  |   ${results[i].UPNO}   |   ${results[i].CHECKOUT}   |   ${results[i].UPWD}`);
-      }
+      console.table(results);
     });
 }
 
