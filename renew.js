636101e2db6d52baf9526014540f160c146712f1
Storyos <storyoser@pukyong.ac.kr>
2024-02-16 15:47:28 +0900

    KBG_연장신청 합치기

--- renew.js
+++ renew.js
@@ -58,3 +58,4 @@ async function renewBook() {
     }
 }
 
+module.exports= {renewBook};
--- search_mycheckout.js
+++ search_mycheckout.js
@@ -1,6 +1,6 @@
 const exp = require('constants');
 const Input = require('./userInput');
-
+const renews = require('./renew');
 async function search_mycheckout(connection, login_success){
     let sql = `select checkout_uid 대출번호, book_uid 도서번호, user_uid 회원번호,DATE_Format(loan_date,"%Y-%m-%D") 대출일, DATE_Format(return_date,"%Y-%m-%D") 반납일, book_delay 반납여부 
     from checkout where user_uid = (select uid from user where unum= ?);`
@@ -8,9 +8,12 @@ async function search_mycheckout(connection, login_success){
     if(err) return console.error(err.message);
     console.table(result);
     });
-    console.log("1. 돌아가기 2. 종료")
+    console.log("1. 돌아가기 2. 연장 신청 3. 종료")
     let search_menu = await Input.getUserInput();
     if(search_menu==='2'){
+        await renews.renewBook();
+    }
+    else if(search_menu==='3'){
         connection.end();
         process.exit();
     }
